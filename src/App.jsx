47258a9a051321777f71b1082c5400df83aa33a3
JEE <jeerawut.jd40@gmail.com>
2023-11-08 22:34:51 +0700

    First step wireframe

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,23 @@
 function App() {
   
 
-  return (
-    <center><p>Coming Soon...</p></center>
-  )
+  return <main>
+    <div id="game-container">
+      <ol id ="players">
+        <li>
+          <span className="player-name">Player 1</span>
+          <span className="player-symbol">X</span>
+        </li>
+        <li>
+          <span className="player-name">Player 2</span>
+          <span className="player-symbol">X</span>
+        </li>
+      </ol>
+
+      GAME BOARD
+    </div>
+    LOG
+  </main>
 }
 
 export default App
