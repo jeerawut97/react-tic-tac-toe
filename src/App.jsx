8111e74e7868d160eac4eed2d5bb8329543dc9d7
JEE <jeerawut.jd40@gmail.com>
2023-11-09 21:20:38 +0700

    User Input & Two Way Binding

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ function App() {
     <main>
       <div id="game-container">
         <ol id ="players">
-          <Player name="Player 1" symbol="X" />
-          <Player name="Player 2" symbol="O" />
+          <Player initName="Player 1" symbol="X" />
+          <Player initName="Player 2" symbol="O" />
         </ol>
         GAME BOARD
       </div>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,22 +1,27 @@
 import { useState } from 'react';
 
-export default function Player({name, symbol}) {
+export default function Player({initName, symbol}) {
+    const [ playerName, setPlayerName ] = useState(initName);
     const [ isEditing, setIsEditing ] = useState(false);
 
     function handleEditClick() {
         setIsEditing((editing) => !editing);
     }
 
-    let playerName = <span className='player-name'>{name}</span>;
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    let editPlayerName = <span className='player-name'>{playerName}</span>;
     if (isEditing) {
-        playerName = <input type="text" required value={name}/>;
+        editPlayerName = <input required type="text" value={playerName} onChange={handleChange}/>;
 
     }
 
     return (
         <li>
             <span className="player">
-                {playerName}
+                {editPlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{isEditing? 'Save': 'Edit'}</button>
