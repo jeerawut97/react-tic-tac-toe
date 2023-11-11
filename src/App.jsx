281ba95c9ebc2508c6bee024683fafcbccff7b57
JEE <jeerawut.jd40@gmail.com>
2023-11-11 13:47:54 +0700

    Deriving State From Props & Sharing State Across Components

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,28 @@ import { useState } from "react";
 
 import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
+import Log from './components/Log.jsx';
 
 function App() {
+  const [gameTurns, setGameTurns ] = useState([]);
   const [activePlayer, setActivePlayer] = useState('X');
 
-  function handleSelectSquare() {
-    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O': 'X');
+  function handleSelectSquare(rowIndex, colIndex) {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O': 'X'));
+    setGameTurns((prevTurns) => {
+      let currentPlayer = 'X';
+
+      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
+        currentPlayer = 'O';
+      }
+
+      const updatedTurns = [
+        {square: {row: rowIndex, col: colIndex}, player: currentPlayer},
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
   }
 
   return (
@@ -17,8 +33,11 @@ function App() {
           <Player initName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
           <Player initName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+        <GameBoard 
+          onSelectSquare={handleSelectSquare}
+          turns={gameTurns} />
       </div>
+      <Log turns={gameTurns}/>
     </main>
   );
 }
